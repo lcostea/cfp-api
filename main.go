dbbe65490e5a14c65ded95c4388065c3bba9d0f2
Liviu Costea <email.lcostea@gmail.com>
2019-06-19 17:59:12 +0300

    Add health and liveness checks

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"log"
-	"net/http"
-
+	"github.com/google/logger"
 	"github.com/gorilla/mux"
 	"github.com/lcostea/cfp-api/pkg/conference"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func getOpenCallForPapers(w http.ResponseWriter, r *http.Request) {
-	cfp := conference.CallForPapers{}
-	respondWithJSON(w, http.StatusOK, cfp)
+	logger.Info("Received a call to /callforpapers ")
+	var cfps [2]conference.CallForPapers
+	cfps[0] = conference.CallForPapers{
+		ConferenceID:   "devseccon",
+		ConferenceName: "DevSecCon London 2019",
+		URL:            "https://www.devseccon.com/call-for-proposals/",
+		Deadline:       time.Date(2019, 07, 14, 0, 0, 0, 0, time.UTC),
+		Description:    "Making development and delivery more secure, without compromise.",
+		Starts:         time.Date(2019, 11, 14, 0, 0, 0, 0, time.UTC),
+	}
+
+	cfps[1] = conference.CallForPapers{
+		ConferenceID:   "blndevops",
+		ConferenceName: "DevOps Days Berlin",
+		URL:            "https://www.papercall.io/2019-berlin",
+		Deadline:       time.Date(2019, 8, 1, 0, 0, 0, 0, time.UTC),
+		Description:    "DevOpsDays is a worldwide series of technical conferences covering topics of working together, software development, IT infrastructure operations, and the intersection between them",
+		Starts:         time.Date(2019, 11, 27, 0, 0, 0, 0, time.UTC),
+	}
+
+	respondWithJSON(w, http.StatusOK, cfps)
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -19,13 +39,26 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+}
 
+func ready(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Received a call to /ready ")
+	time.Sleep(5 * time.Second)
+}
+
+func healthy(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Received a call to /healthy ")
+	time.Sleep(1 * time.Second)
 }
 
 func main() {
+	loggerStd := logger.Init("LoggerExample", true, false, ioutil.Discard)
 	r := mux.NewRouter()
-	r.HandleFunc("/callforpapers", getOpenCallForPapers).Methods("GET")
+	r.HandleFunc("/callforpapers", getOpenCallForPapers)
+	r.HandleFunc("/ready", ready)
+	r.HandleFunc("/healthy", healthy)
+
 	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatal(err)
+		loggerStd.Fatal(err)
 	}
 }
